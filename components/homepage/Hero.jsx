e3e8c76b39fdc83bf3caac0f58b897fc69c88412
bsegers <briansegs@gmail.com>
2024-06-14 18:53:28 -0400

    feat: Add particales and avatar to hero section

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -1,9 +1,39 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+import particlesOptions from "../../constants/particles_config.json";
 import ButtonDark from "../ButtonDark";
+import avatar from "../../assets/hero/avatar_03.png";
 
 const Hero = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    if (init) {
+      return;
+    }
+
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
   return (
     <section className="max-container flex min-h-screen w-full flex-col justify-center gap-10 xl:flex-row">
-      <div className="relative flex min-h-screen w-full bg-hero bg-center bg-no-repeat"></div>
+      <div className="relative flex min-h-screen w-full bg-hero bg-center bg-no-repeat">
+        {init && <Particles options={particlesOptions} />}
+
+        <Image
+          src={avatar}
+          alt="avatar"
+          className="absolute bottom-0 left-[5%] right-0 mx-auto w-[95%] object-contain mix-blend-screen"
+        />
+      </div>
 
       <div className="max-xl:padding-x relative flex w-full flex-col items-start justify-center pt-28 xl:w-2/5">
         <p className="text-[28px] tracking-[-1px] text-red-400">
