176c1149c04b6ff133c1bdd98d49fc26200fc81e
bsegers <briansegs@gmail.com>
2024-06-25 10:59:53 -0400

    Add projects page

---
+++ app/projects/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <section className="xl:padding-x">
+      <div className="max-container min-h-screen w-full">
+        <div className="padding">Projects</div>
+      </div>
+    </section>
+  );
+};
+
+export default page;
