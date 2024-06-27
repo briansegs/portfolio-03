051a4a93a81a1d51c00f915ae47bc4002f47fbd6
bsegers <briansegs@gmail.com>
2024-06-27 09:59:37 -0400

    doc: Add comments

--- components/projects/ProjectSection.jsx
+++ components/projects/ProjectSection.jsx
@@ -1,15 +1,18 @@
 "use client";
 
 import { useState } from "react";
-import { projects } from "@/constants";
 import Image from "next/image";
+
 import RoundArrowButton from "@/components/projects/RoundArrowButton";
+import ProjectButton from "./ProjectButton";
+import Chip from "../Chip";
+
 import { PiArrowFatLeftFill, PiArrowFatRightFill } from "react-icons/pi";
 import { CgLinear } from "react-icons/cg";
-import Chip from "../Chip";
-import ProjectButton from "./ProjectButton";
 import { RxGithubLogo } from "react-icons/rx";
 import { LuMonitor } from "react-icons/lu";
+
+import { projects } from "@/constants";
 
 const ProjectCard = () => {
   const [project, setProject] = useState(projects[0]);
@@ -29,11 +32,12 @@ const ProjectCard = () => {
 
   return (
     <>
+      {/* Projects Menu */}
       <div className="flex gap-4 max-sm:flex-col max-sm:items-center">
         {projects.map((p, idx) => (
           <p
             key={p.id}
-            className={` cursor-pointer font-primary text-xl font-bold tracking-[-0.4px] duration-500 ease-out ${p.id === project.id ? "text-white" : "text-white/40"} hover:text-white`}
+            className={`cursor-pointer font-primary text-xl font-bold tracking-[-0.4px] duration-500 ease-out ${p.id === project.id ? "text-black" : "text-black/40"} hover:text-black`}
             onClick={() => {
               setProject(projects[idx]);
               setIndex(idx);
@@ -44,29 +48,38 @@ const ProjectCard = () => {
         ))}
       </div>
 
+      {/* Left Arrow Button */}
       <div className="mt-7 flex w-full items-center justify-between">
         <RoundArrowButton
           icon={<PiArrowFatLeftFill />}
           onClick={() => handleLeftClick()}
         />
 
+        {/* Project Card */}
         <div className="flex min-h-[598px] w-3/4 flex-col-reverse gap-8 rounded-md bg-white p-6 max-sm:w-full xl:flex-row ">
+          {/* Left Text */}
           <div className="flex w-full flex-col justify-between gap-6 xl:w-2/5">
             <div className="flex flex-col justify-between gap-6">
+              {/* Project Type Chip */}
               <Chip
                 chipStyles="bg-slate-300"
                 textStyles="text-black text-base sm:text-lg"
-                text="Front-End Web Development"
+                text={project.type}
                 icon={<CgLinear />}
               />
+
+              {/* Project Title */}
               <div className="mb-8">
                 <p className="text-5xl tracking-[-1px]">{project.title}</p>
               </div>
 
               <div className="min-h-[228px]">
+                {/* Project Description Title */}
                 <p className="mb-2 font-primary text-lg font-extrabold tracking-[-0.4px]">
                   {project.descriptionTitle}
                 </p>
+
+                {/* Project Description */}
                 <p className="font-primary text-lg leading-6 tracking-[-0.4px]">
                   {project.description}
                 </p>
@@ -74,6 +87,7 @@ const ProjectCard = () => {
             </div>
 
             <div className="flex flex-col justify-between gap-6">
+              {/* Project Tech Stack */}
               <div className="flex min-h-20 flex-wrap items-end gap-2 whitespace-nowrap">
                 {project.stack.map((tech, index) => (
                   <Chip
@@ -84,6 +98,8 @@ const ProjectCard = () => {
                   />
                 ))}
               </div>
+
+              {/* Project Links */}
               <div className="flex gap-4 border-t-2 border-black pt-4">
                 <div className="">
                   <ProjectButton
@@ -103,6 +119,7 @@ const ProjectCard = () => {
             </div>
           </div>
 
+          {/* Right Image */}
           <div className="w-full xl:w-3/5">
             <Image
               src={project.imgUrl}
@@ -112,6 +129,7 @@ const ProjectCard = () => {
           </div>
         </div>
 
+        {/* Right Arrow Button */}
         <RoundArrowButton
           icon={<PiArrowFatRightFill />}
           onClick={() => handleRightClick()}
