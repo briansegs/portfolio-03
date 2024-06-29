ff2d7954463eb7a8b9fedaddb616d34d42ac806c
bsegers <briansegs@gmail.com>
2024-06-29 15:49:08 -0400

    feat: Add error styles

--- components/contact/ContactInput.jsx
+++ components/contact/ContactInput.jsx
@@ -1,29 +1,44 @@
+"use client";
+
+import { useState } from "react";
+
+const errorStyles = "outline outline-2 outline-offset-1 outline-primary ";
+
 const styles =
   "w-full rounded-sm border-b-2 border-black px-3 pt-2 text-2xl tracking-[-0.5px] ring-secondary ring-offset-white placeholder:text-slate-400 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2";
 
-const inputHeight = "h-[66px]";
+const inputHeight = "h-[68px]";
 
 const ContactInput = ({
   type,
   placeholder,
   warning,
-  onchange,
+  onChange,
   id,
-  isValid,
-}) => (
-  <div className={inputHeight}>
-    <input
-      id={id}
-      required
-      className={styles}
-      type={type}
-      placeholder={placeholder}
-      onChange={onchange}
-    />
-    {!isValid && (
-      <p className="font-primary font-semibold text-primary">{warning}</p>
-    )}
-  </div>
-);
+  isInvalid,
+  value,
+}) => {
+  const [visited, setVisited] = useState(false);
+
+  const showError = () => isInvalid && visited;
+
+  return (
+    <div className={inputHeight}>
+      <input
+        id={id}
+        required
+        className={`${styles} ${showError() && errorStyles}`}
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+        value={value}
+        onBlur={() => setVisited(true)}
+      />
+      {showError() && (
+        <p className="font-primary font-semibold text-primary">{warning}</p>
+      )}
+    </div>
+  );
+};
 
 export default ContactInput;
--- components/contact/ContactTextarea.jsx
+++ components/contact/ContactTextarea.jsx
@@ -1,17 +1,32 @@
+"use client";
+
+import { useState } from "react";
+
+const errorStyles = "outline outline-2 outline-offset-1 outline-primary ";
+
 const styles =
   "w-full rounded-sm border-b-2 border-black px-3 pt-2 text-2xl tracking-[-0.5px] ring-secondary ring-offset-white placeholder:text-slate-400 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2";
 
-const ContactTextarea = ({ warning, onchange, id }) => (
-  <div className="h-[351px]">
-    <textarea
-      id={id}
-      required
-      placeholder="Message"
-      className={`min-h-80 ${styles}`}
-      onChange={onchange}
-    />
-    <p className="hidden font-primary font-semibold text-primary">{warning}</p>
-  </div>
-);
+const ContactTextarea = ({ warning, onChange, id, isInvalid, value }) => {
+  const [visited, setVisited] = useState(false);
 
+  const showError = () => isInvalid && visited;
+
+  return (
+    <div className="h-[355px]">
+      <textarea
+        id={id}
+        required
+        placeholder="Message"
+        className={`min-h-80 ${styles} ${showError() && errorStyles}`}
+        onChange={onChange}
+        onBlur={() => setVisited(true)}
+        value={value}
+      />
+      {showError() && (
+        <p className="font-primary font-semibold text-primary">{warning}</p>
+      )}
+    </div>
+  );
+};
 export default ContactTextarea;
