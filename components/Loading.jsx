3bcf1702bc18c13d196ee047efffb5935985d58e
bsegers <briansegs@gmail.com>
2024-06-29 00:02:18 -0400

    feat: Style Loading component

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,5 +1,7 @@
-const Loading = () => {
-  return <div>Loading</div>;
-};
+const Loading = () => (
+  <div className="flex h-[617px] w-4/5 items-center justify-center sm:h-[541px] xl:w-2/5">
+    <h1 className="text-4xl text-slate-400">Loading...</h1>
+  </div>
+);
 
 export default Loading;
