cb190fd1457353f7deecfd36c62e5ec4118dfc5b
bsegers <briansegs@gmail.com>
2024-07-02 21:23:23 -0400

    feat: Add grid 4 3d-style flip

--- components/contact/artGrid/Grid4.jsx
+++ components/contact/artGrid/Grid4.jsx
@@ -1,3 +1,14 @@
-const Grid4 = () => <div>Grid4</div>;
+const Grid4 = () => (
+  <div className="card-3d absolute size-full border border-black">
+    <div className="card-3d-front absolute flex size-full items-center justify-center">
+      <p className="font-primary text-6xl font-black tracking-[-3px]">Hello</p>
+    </div>
+    <div className="card-3d-back absolute flex size-full items-center justify-center">
+      <p className="font-primary text-6xl font-black tracking-[-3px]">
+        Goodbye
+      </p>
+    </div>
+  </div>
+);
 
 export default Grid4;
