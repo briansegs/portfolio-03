55cabff048f297ccebc178220b63a157e6b8ce1d
bsegers <briansegs@gmail.com>
2024-07-03 17:17:17 -0400

    fix: bg position/img | button hover color

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
       backgroundImage: {
         hero: "url('/bg_01.png')",
         about: "url('/about_bg.png')",
-        projects: "url('/bg_05.png')",
+        projects: "url('/bg_06.png')",
         grid1: "url('/frame_01.png')",
         grid3: "url('/frame_03.png')",
       },
