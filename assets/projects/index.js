402bfdcf869ba29a77ef968f877312c0c52cd278
bsegers <briansegs@gmail.com>
2024-07-04 18:08:48 -0400

    feat: Add project 3 image

--- assets/projects/index.js
+++ assets/projects/index.js
@@ -1,6 +1,6 @@
 import img01 from "./img-01.jpg";
 import img02 from "./img-02.jpg";
-import img03 from "./img-03.jpg";
+import img03 from "./project3.png";
 import img04 from "./project4.png";
 import img05 from "./project5.png";
 
