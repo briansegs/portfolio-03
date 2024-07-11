0b4b1de7ad720895ae76ac53508e1ac2b49cee69
bsegers <briansegs@gmail.com>
2024-07-11 09:57:31 -0400

    fix: Section spacing

--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,10 @@ import { About, CTA, Hero, Projects, Services } from "@/components/homepage";
 export default function Home() {
   return (
     <>
-      <section className="xl:padding-l wide:padding-r padding-b">
+      <section className="relative mb-12 sm:mb-24">
         <Hero />
       </section>
-      <section className="padding-x py-10">
+      <section className="padding-x py-8 sm:py-12">
         <Services />
       </section>
       <section className="padding">
