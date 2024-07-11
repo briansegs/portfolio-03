2a4ddad616b2573b8cfa1c73162e63911e4ef4fe
bsegers <briansegs@gmail.com>
2024-07-11 10:00:09 -0400

    fix: Make bigger

--- components/contact/contactForm/ContactButton.jsx
+++ components/contact/contactForm/ContactButton.jsx
@@ -6,7 +6,7 @@ const ContactButton = ({ disabled, isLoading }) => (
   <button
     type="submit"
     disabled={disabled}
-    className="flex w-full items-center justify-center whitespace-nowrap rounded-md bg-black p-[12px] font-primary text-xl font-bold capitalize tracking-[-0.4px] text-white ring-offset-white transition duration-200 hover:-translate-y-1 hover:translate-x-1 hover:drop-shadow-[-4px_4px_0_rgba(248,113,113,1)] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-300 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-65"
+    className="flex w-full items-center justify-center whitespace-nowrap rounded-lg bg-black p-[16px] font-primary text-xl font-extrabold capitalize tracking-[-0.4px] text-white ring-offset-white transition duration-200 hover:-translate-y-1 hover:translate-x-1 hover:drop-shadow-[-5px_5px_0_rgba(248,113,113,1)] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-300 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-65"
   >
     {isLoading ? <LoadingSpinner /> : "Send message"}
   </button>
