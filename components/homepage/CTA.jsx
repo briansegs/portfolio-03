8a65f5acf2baf267c10f043de2ea7230e04beaba
bsegers <briansegs@gmail.com>
2025-04-15 19:19:52 -0400

    Use Image instead of tailwind background

--- components/homepage/CTA.jsx
+++ components/homepage/CTA.jsx
@@ -1,20 +1,30 @@
 import Link from "next/link";
 import ButtonDark from "../ButtonDark";
 import { ctaData } from "@/constants";
+import Image from "next/image";
+import bgHero from "@/assets/hero/bgHero.webp";
 
 const { headingText, subtext, buttonText } = ctaData;
 
 const CTA = () => (
   <section className="max-container w-full">
-    <div className="flex flex-col items-center justify-between gap-8 rounded-lg bg-hero p-8 xl:flex-row">
-      <dir className="flex flex-col gap-6">
+    <div className="relative flex flex-col items-center justify-between gap-8 overflow-hidden rounded-lg p-8 xl:flex-row">
+      <Image
+        src={bgHero}
+        alt="Cta background"
+        fill
+        quality={75}
+        className="-z-10 object-cover object-center"
+      />
+
+      <div className="flex flex-col gap-6">
         <h2 className="whitespace-normal text-center font-primary text-4xl font-black tracking-[-2px] text-white sm:text-6xl sm:tracking-[-3px] xl:max-w-lg xl:whitespace-nowrap xl:text-left">
           {headingText}
         </h2>
         <p className="w-full text-center font-primary text-base font-semibold text-slate-100 xl:w-1/2 xl:text-left xl:text-lg">
           {subtext}
         </p>
-      </dir>
+      </div>
 
       <div className="whitespace-nowrap">
         <Link href={"/contact"}>
